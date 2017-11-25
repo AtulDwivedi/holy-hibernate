b982e43b6b682572457eb306a56e606083eb6e26
Atul Dwivedi <er.dwivediatul@gmail.com>
2017-11-25 09:13:04 +0530

    manytomany-bi

--- src/com/atuldwivedi/learn/hibernate/association/manytomany/bi/ManyToManyBiApp.java
+++ src/com/atuldwivedi/learn/hibernate/association/manytomany/bi/ManyToManyBiApp.java
@@ -1,10 +1,13 @@
-package com.atuldwivedi.learn.hibernate.association.manytomany.uni;
+package com.atuldwivedi.learn.hibernate.association.manytomany.bi;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class ManyToManyApp {
+public class ManyToManyBiApp {
 
 	static SessionFactory sessionFacoty = null;
 	static Session session = null;
@@ -12,10 +15,10 @@ public class ManyToManyApp {
 	public static void main(String[] args) {
 
 		try {
-			sessionFacoty = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Person.class)
+			sessionFacoty = new Configuration().configure("/com/atuldwivedi/learn/hibernate/association/manytomany/bi/hibernate.cfg.xml").addAnnotatedClass(Person.class)
 					.addAnnotatedClass(Address.class).buildSessionFactory();
 			session = sessionFacoty.getCurrentSession();
-			
+
 //			insert();
 			remove();
 
@@ -28,31 +31,35 @@ public class ManyToManyApp {
 	public static void insert() {
 		session.beginTransaction();
 
-		Person person1 = new Person("Atul");
-		Person person2 = new Person("At");
+		Person person1 = new Person("ABC-123");
+		Person person2 = new Person("DEF-456");
 
 		Address address1 = new Address("12th Avenue", "12A");
 		Address address2 = new Address("18th Avenue", "18B");
 
-		person1.getAddresses().add(address1);
-		person1.getAddresses().add(address2);
+		person1.addAddress(address1);
+		person1.addAddress(address2);
 
-		person2.getAddresses().add(address1);
+		person2.addAddress(address1);
 
-		session.save(person1);
-		session.save(person2);
+//		session.save(person1);
+//		session.save(person2);
+				
+		session.save(address1);
+		session.save(address2);
 
 		session.getTransaction().commit();
 	}
 
 	public static void remove() {
 		session.beginTransaction();
-		
+
 		Person person1 = session.get(Person.class, 1l);
 		Address address1 = session.get(Address.class, 1l);
-		
-		person1.getAddresses().remove(address1);
+		Address address2 = session.get(Address.class, 1l);
 
+		person1.getAddresses().remove(address1);
+		
 		session.getTransaction().commit();
 	}
 }
--- src/com/atuldwivedi/learn/hibernate/association/manytomany/bi/manytomany-bi.sql
+++ src/com/atuldwivedi/learn/hibernate/association/manytomany/bi/manytomany-bi.sql
@@ -1,6 +1,6 @@
---DROP SCHEMA IF EXISTS hfw_manytomany_uni;
+--DROP SCHEMA IF EXISTS hfw_manytomany_bi;
 
---CREATE SCHEMA hfw_manytomany_uni;
+--CREATE SCHEMA hfw_manytomany_bi;
 
 DROP TABLE IF EXISTS Address;
 DROP TABLE IF EXISTS Person;
@@ -20,7 +20,7 @@ CREATE TABLE Person (
 );
 
 CREATE TABLE Person_Address (
-    Person_id BIGINT NOT NULL ,
+    Owners_id BIGINT NOT NULL ,
     addresses_id BIGINT NOT NULL
 );
 
@@ -29,5 +29,5 @@ ADD CONSTRAINT addresses_id_fk
 FOREIGN KEY (addresses_id) REFERENCES Address;
 
 ALTER TABLE Person_Address
-ADD CONSTRAINT Person_id_fk
-FOREIGN KEY (Person_id) REFERENCES Person;
+ADD CONSTRAINT Owners_id_fk
+FOREIGN KEY (Owners_id) REFERENCES Person;
